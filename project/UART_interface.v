c47c892779f510edf686cb6ef867d579ffc5685a
nayera540 <69148381+nayera540@users.noreply.github.com>
2023-01-05 17:50:38 +0200

    changes

--- project/UART_interface.v
+++
@@ -1,8 +0,0 @@
-module UART #(parameter clk_rate = 100000000, parameter baud_rate = 9600)
-  (
-    input wire clk,
-    input wire rst,
-    input wire rx,
-    input wire rxEn,
-    output wire [7:0]
-  );
